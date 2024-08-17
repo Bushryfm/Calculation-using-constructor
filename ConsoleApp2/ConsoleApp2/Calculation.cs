2de6e6a2651cf7d8920930e4055d4da504f0b101
F m Bushry <121561467+Bushryfm@users.noreply.github.com>
2024-08-17 11:04:11 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Calculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Calculation
+    {
+       
+
+        internal int value1, value2;
+        internal Calculation(int a, int b)
+        {
+            value1 = a;
+            value2 = b;
+
+        }
+        internal int Addition()
+        {
+            return value1 + value2;
+        }
+
+        internal int Subtraction()
+        {
+            return value1 - value2;
+        }
+
+        internal int Multiplication()
+        {
+        return value1 - value2;
+        }
+
+        internal int Division()
+        { 
+        return value1 -value2;
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2;
+
+Calculation obj = new Calculation(500,50);
+
+
+
+
+Console.WriteLine("the addition is " +obj.Addition());
+Console.WriteLine("the Subtraction is " +obj.Subtraction());
+Console.WriteLine("the Multiplication  is " +obj.Multiplication());
+Console.WriteLine("the Division is "+obj.Division());
+
